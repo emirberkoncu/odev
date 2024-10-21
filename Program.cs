64d9736bfaeeb683dcf86f85d4b16f768f680555
Emir Berk Oncu <emirberkoncu@gmail.com>
2024-10-21 13:14:01 +0300

    csharp

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace odev
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+
+      // String degisken = " ";
+      // Console.WriteLine(degisken);
+      // if(degisken==""){
+      //   Console.WriteLine("True");
+      // }else {
+      //   Console.WriteLine("False");
+      // }
+      // byte b = 5; //! 1 byte -128 + 127
+      // sbyte sb = 5; //! 1 byte
+      // short s = 5; //! 2 byte -32768 + 32767 
+      // ushort us = 5; //! 2 byte 0 - 65535
+
+      // Int16 i16 = 2; //! 2 byte
+      // int i = 2; //! 4 byte -2,147,483,648 + 2,147,483,647
+      // Int32 i32 = 2; //! 4 byte
+      // Int64 i64 = 2; //! 8 byte -9,223,372,036,854,775,808 + 9,223,372,036,854,775,807
+
+      // uint ui = 2 ; //! 4 byte
+      // long l = 4; //! 8 byte
+      // ulong ul = 4; //! 8 byte
+
+      // float f = 5; //! 4 byte
+      // double d = 5; //! 8 byte
+      // decimal de = 5; //! 16 byte
+
+      // char c = 'c'; //! 2 byte
+      // string str = "string"; //! sınırsız
+
+      // bool b1 = true; //! 1 byte
+      // bool b2 = false; //! 1 byte
+
+      // DateTime dt = DateTime.Now; //! 8 byte
+      // Console.WriteLine(dt);
+
+      // object o1 = "string";
+      // object o2 = 'y';
+      // object o3 = 5;
+      // object o4 = 4.5;
+
+      // // string 
+      // string str1 = string.Empty;
+      // str1 = "Berk Oncu";
+      // string ad = "Emir";
+      // string tam_isim = ad + " " + str1;
+      // Console.WriteLine(tam_isim);
+
+      // // integer
+      // int integer1 = 5;
+      // int interger2 = 3;
+      // int integer3 = integer1 * interger2; 
+
+      // // boolean 
+      // bool bool1 = 10 < 2;
+      // // Console.WriteLine(bool1);
+
+      // // Degisken Donusumleri
+      // string str20 = "20";
+      // int int20 = 20;
+      // string yeniDeger = str20 + int20.ToString();
+      // Console.WriteLine(yeniDeger);
+
+      // // int int21 = int20 + Convert.ToInt32(str20);
+      // // Console.WriteLine(int21);
+
+      // int int22 = int20 + int.Parse(str20);
+      // Console.WriteLine(int22);
+
+      // // datetime 
+      // string datetime = DateTime.Now.ToString("dd/MM/yyyy");
+      // Console.WriteLine(datetime);
+
+      // string hour = DateTime.Now.ToString("HH:mm");
+      // Console.WriteLine(hour);
+
+      // string fulltime = datetime + " " + hour;
+      // Console.WriteLine(fulltime);
+
+    }
+  }
+}
